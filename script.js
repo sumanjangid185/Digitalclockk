e4e2939bf68ab2f08a2e3ede07e65fb2ca2a8749
Suman Jangid <149228213+sumanjangid185@users.noreply.github.com>
2024-04-20 14:52:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let hrs=document.getElementById("hrs");
+// let min=document.getElementById("min");
+// let sec=document.getElementById("sec");
+
+// let currentTime= new Date();
+//    console.log(currentTime.getMinutes);
+
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+setInterval(()=>{
+    let currentTime = new Date();
+    // console.log(currentTime.getMinutes()); // Add parentheses to invoke the getMinutes method
+    hrs.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getHours();
+    min.innerHTML=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+    sec.innerHTML=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+},1000)
+
